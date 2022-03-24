a3d512600944fe1642b15b977f2b81596decf6e0
Alen Jo <alenjo874@gmail.com>
2022-03-24 13:50:36 -0400

    added login form

---
+++ client/src/components/UserLogIn/LogInForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function LogInForm() {
+  return (
+    <div>
+        <form>
+            <label>Sign In</label>
+            <input></input>
+            <label>Password</label>
+            <input></input>
+        </form>
+    </div>
+  )
+}
+
+export default LogInForm
