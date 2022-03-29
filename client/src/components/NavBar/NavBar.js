de192da1524d64e3b1789da5890867934c0992d9
Alen Jo <alenjo874@gmail.com>
2022-03-29 15:31:54 -0400

    ham nav functionality

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,40 +1,62 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion/dist/framer-motion";
+import { AnimatePresence, motion } from "framer-motion/dist/framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 
 function NavBar({ blogsArray }) {
   const [showHamNav, setShowHamNav] = useState(false);
+  function handleShowHam() {
+    setShowHamNav(true);
+  }
 
+  function handleHideHam() {
+    setShowHamNav(false);
+  }
+
+ 
   const navHam = (
-    <motion.div className="ham-elements-container">
+    <motion.div
+      className="ham-elements-container"
+      initial={{ y: "-10%", opacity: 0 }}
+      animate={{
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 0.25,
+          type: "show",
+          ease: "easeIn",
+        },
+      }}
+      exit={{
+        y: "-10%",
+        opacity: 0,
+        transition: { duration: 0.25, ease: "easeOut" },
+      }}
+    >
       <ul>
         <Link to="/">
-          <li>Home</li>
+          <li onClick={handleHideHam}>Home</li>
         </Link>
         <Link to="/blogs">
-          <li>Blogs</li>
+          <li onClick={handleHideHam}>Blogs</li>
         </Link>
         <Link to="/profile">
-          <li>Profile</li>
+          <li onClick={handleHideHam}>Profile</li>
         </Link>
       </ul>
     </motion.div>
   );
 
-  function handleHamNav() {
-    setShowHamNav(true);
-  }
-
+ 
   const triBarButton = (
-    <motion.button className="nav-hamburger-btn" onClick={handleHamNav}>
+    <motion.button className="nav-hamburger-btn" onClick={handleShowHam}>
       <FontAwesomeIcon icon={faBars} />
     </motion.button>
   );
 
   const xButton = (
-    <motion.button className="nav-hamburger-btn">
+    <motion.button className="nav-hamburger-btn" onClick={handleHideHam}>
       <FontAwesomeIcon icon={faX} />
     </motion.button>
   );
@@ -58,9 +80,11 @@ function NavBar({ blogsArray }) {
             </Link>
           </ul>
         </div>
-        <div className="nav-hamburger-container">{triBarButton}</div>
+        <div className="nav-hamburger-container">
+          {showHamNav ? xButton : triBarButton}
+        </div>
       </div>
-      {showHamNav ? navHam : null}
+      <AnimatePresence>{showHamNav ? navHam : null}</AnimatePresence>
     </div>
   );
 }
